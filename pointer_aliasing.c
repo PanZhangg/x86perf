8ad4daa10ff5d11946e92a74b6df55762d48ddbc
PanZhangg <dazhangpan@gmail.com>
2018-12-11 07:59:32 +0800

    modify

--- pointer_aliasing.c
+++ pointer_aliasing.c
@@ -18,7 +18,7 @@ pointer_aliasing_f(uint32_t *a, uint32_t *b)
 {
     uint64_t n = ARRAY_LEN;
     while (n--) {
-        *(a++) = *(b++);
+        *(a++) = *b + 1;
     }
 }
 #endif
@@ -29,7 +29,7 @@ pointer_aliasing_f(uint32_t *__restrict a, uint32_t *__restrict b)
 {
     uint64_t n = ARRAY_LEN;
     while (n--) {
-        *(a++) = *(b++);
+        *(a++) = *b + 1;
     }
 }
 #endif
@@ -38,18 +38,31 @@ int
 main(void)
 {
     uint32_t *arrayA = (uint32_t *)malloc(ARRAY_LEN * sizeof(uint32_t));
-    uint32_t *arrayB = (uint32_t *)malloc(ARRAY_LEN * sizeof(uint32_t));
+
+    int i = 0;
+    for(; i < ARRAY_LEN; i++) {
+            arrayA[i] = i;
+    }
+
+#ifdef PTR_ALIAS
+    uint32_t *p = &arrayA[8];
+#endif
+
+#ifdef ANTI_PTR_ALIAS
+    uint32_t v = 8;
+    uint32_t *p = &v;
+#endif
 
     /*warm up*/
-    //pointer_aliasing_f(arrayA, arrayB);
+    pointer_aliasing_f(arrayA, p);
 
     uint64_t start = trace_cpu_time_now();
 
-    pointer_aliasing_f(arrayA, arrayB);
+    pointer_aliasing_f(arrayA, p);
 
     uint64_t end = trace_cpu_time_now();
 
-    printf("CPU cycles: %ld\n", end - start);
+    printf("arrayA[8] = %d, CPU cycles: %ld\n", arrayA[8], end - start);
 
     return 0;
 }
