e0341f806d513b1d84197dc02dd22b63c19820c1
PanZhangg <dazhangpan@gmail.com>
2018-12-26 15:32:56 +0800

    indirectcall

--- indirectcall.c
+++ indirectcall.c
@@ -4,13 +4,22 @@
 #include "benchmark.h"
 
 #define OP_TYPE_NUM 5
+#define RAND_ARRAY_LEN 1024
 
-typedef void (*open)(void);
-typedef void (*close)(void);
-typedef void (*write)(void);
-typedef void (*read)(void);
-typedef void (*add)(void);
-typedef void (*delete)(void);
+#define FUNC_RETURN_TYPE void
+#define FUNC_PARAMETER void
+
+#define OPS \
+    _(open) \
+    _(close) \
+    _(write) \
+    _(read) \
+    _(add) \
+    _(delete)
+
+#define _(op) typedef FUNC_RETURN_TYPE (*op)(FUNC_PARAMETER);
+OPS
+#undef _
 
 enum types {
     red,
@@ -20,15 +29,28 @@ enum types {
     white
 };
 
-void
-open_type_red(void){
-    
-}
+#define LIST \
+    _(red) \
+    _(blue)\
+    _(yellow)\
+    _(black)\
+    _(white)
 
-void
-open_type_blue(void){
-    
-}
+#define FUNC_BODY {int i=0; i++;}
+
+#undef FUNC_RETURN_TYPE
+#define FUNC_RETURN_TYPE static void
+
+#define _(color) FUNC_RETURN_TYPE open_##color(FUNC_PARAMETER) FUNC_BODY \
+    FUNC_RETURN_TYPE close_##color(FUNC_PARAMETER) FUNC_BODY \
+    FUNC_RETURN_TYPE write_##color(FUNC_PARAMETER) FUNC_BODY \
+    FUNC_RETURN_TYPE read_##color(FUNC_PARAMETER) FUNC_BODY \
+    FUNC_RETURN_TYPE add_##color(FUNC_PARAMETER) FUNC_BODY \
+    FUNC_RETURN_TYPE delete_##color(FUNC_PARAMETER) FUNC_BODY
+
+LIST
+
+#undef _
 
 struct op {
     open open_fp;
@@ -41,8 +63,55 @@ struct op {
 
 struct op ops[OP_TYPE_NUM];
 
+int rand_array[RAND_ARRAY_LEN];
+
+static
+void call_ops(void) {
+    int i = 0;
+    for(; i < RAND_ARRAY_LEN; i++) {
+        int idx = rand_array[i];
+        ops[idx].open_fp();
+    }
+}
+
+static
+void call_ops_directly(void) {
+    int i = 0;
+    for(; i < RAND_ARRAY_LEN; i++) {
+        int idx = rand_array[i];
+        switch(idx) {
+        #define _(color) case color: open_##color(); break;
+        LIST
+        #undef _
+            default: open_red(); break;
+        }
+    }
+}
+
 int
 main(void)
 {
+#define _(color) ops[color].open_fp = &open_##color; \
+    ops[color].close_fp = &close_##color; \
+    ops[color].write_fp = &write_##color; \
+    ops[color].read_fp = &read_##color; \
+    ops[color].add_fp = &add_##color; \
+    ops[color].delete_fp = &delete_##color;
+
+LIST
+
+#undef _
+    int i = 0;
+    for(; i < RAND_ARRAY_LEN; i++) {
+        srand((unsigned)time(NULL));
+        rand_array[i] = rand() % OP_TYPE_NUM;
+        rand_array[i] = 1;
+    }
+
+BEST_TIME_NOCHECK(call_ops(), ,1000000, 1, 1);
+
+BEST_TIME_NOCHECK(call_ops_directly(), ,1000000, 1, 1);
+
+
     return 0;
 }
