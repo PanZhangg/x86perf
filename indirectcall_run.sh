412428a14262f7ccc7bfed443dd09ebc804b8ba4
PanZhangg <dazhangpan@gmail.com>
2018-12-29 16:05:37 +0800

    perf events tuning

--- indirectcall_run.sh
+++ indirectcall_run.sh
@@ -2,10 +2,10 @@
 
 #Related raw events of Haswell microarchitecture CPU
 
-EVENT1=' -e cpu/event=0x08,umask=0x01,name=ALL_TLB_MISS_PAGE_WALK/'
-EVENT2=' -e cpu/event=0x3C,umask=0x01,name=UNHALTED_CYC/'
-EVENT3=' -e cpu/event=0x85,umask=0x01,name=ITLB_PAGE_WALK/'
-EVENT4=' -e cpu/event=0x85,umask=0x02,name=ITLB_4K_PAGE_WALK/'
+EVENT1=' -e cpu/event=0x80,umask=0x40,name=ICACHE_STALL/'
+EVENT2=' -e cpu/event=0x83,umask=0x01,name=ICACHE_FETCH_HIT/'
+EVENT3=' -e cpu/event=0x83,umask=0x02,name=ICACHE_FETCH_MISS/'
+EVENT4=' -e cpu/event=0x83,umask=0x04,name=ICACHE_FETCH_TAG_MISS/'
 EVENT5=' -e cpu/event=0x85,umask=0x10,name=ITLB_MISS_DURATION/'
 EVENT6=' -e cpu/event=0x85,umask=0x20,name=ITLB_MISS_HIT_STLB_PAGE_WAKE/'
 EVENT7=' -e cpu/event=0x85,umask=0x60,name=ITLB_MISS_HIT_STLB/'
@@ -14,7 +14,7 @@ EVENT9=' -e cpu/event=0xBC,umask=0x21,name=ITLB_LOAD_HIT/'
 EVENT10=' -e cpu/event=0xA2,umask=0x01,name=STALL_RESOURCE/'
 EVENT11=' -e cpu/event=0xA2,umask=0x04,name=STALL_RS/'
 PERF_STAT='perf stat'
-EVENTS=$EVENT10$EVENT11
+EVENTS=$EVENT1$EVENT2$EVENT3$EVENT4$EVENT10$EVENT11
 PERF_CMD=$PERF_STAT$EVENTS
 
 echo "====RANDOM IDX -O0"
